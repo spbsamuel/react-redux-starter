5be77117764ef3dce7c401c76188ef88652d134f
samuel <spbsamuel@gmail.com>
2018-01-27 19:23:35 +0800

    adds classnames for better css class control
    adds classnames, updates example

--- src/connected_components/Counter/Counter.js
+++ src/connected_components/Counter/Counter.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import cx from 'classnames'
+
 import cls from './Counter.scss'
 
 function Counter({count, increment, decrement}) {
+  const CountClass = cx(
+    cls.Count,
+    {
+      [cls.Negative]: count < 0,
+      [cls.Positive]: count > 0
+    }
+  );
   return (
     <div>
-      <h1>{count}</h1>
+      <h1 className={CountClass}>
+        {count}
+      </h1>
       <button className={cls.CounterBtn} onClick={increment}>Increase by 1</button>
       <button className={cls.CounterBtn} onClick={decrement}>Decrease by 1</button>
     </div>
