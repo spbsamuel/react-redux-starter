bae2b2b46c904ebbfabe994b5c43697c993606c4
samuel <spbsamuel@gmail.com>
2018-01-27 21:06:57 +0800

    adds redux dev tools

--- src/store/storeFactory.js
+++ src/store/storeFactory.js
@@ -1,10 +1,24 @@
-import {createStore} from 'redux';
+import {compose, createStore} from 'redux';
 import rootReducer from './reducers/rootReducer';
 
+function applyReduxDevTools(enhancers) {
+  const devToolsExtension = window.devToolsExtension;
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+
 function storeFactory(initialState = {}) {
+  const enhancers = [];
+
+  if (__DEV__) applyReduxDevTools(enhancers);
+
   return createStore(
     rootReducer,
     initialState,
+    compose(
+      ...enhancers
+    )
   );
 }
 
